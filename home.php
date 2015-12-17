747b83e681a23ecfe4f27f98f9d965b71691c90f
HaniK22 <Hani@fallound.com>
2015-12-17 18:59:43 +0200

    Added home page
    home page

--- home.php
+++ home.php
@@ -1,23 +1,43 @@
-<!DOCTYPE html>
 <html>
 <head>
-<meta charset='utf-8'>
-<link href="styles/navbar.css" type="text/css" rel="stylesheet" /> 
+    <link href="styles/home.css" rel="stylesheet" type="text/css">
 
 </head>
-<body>
-	<div id = "navbarcontainer">
-		<h1><a class="active" href="home.php">Home</a></h1>
-		<ul>
-			<li><h1><a href="#">Movies</a></h1>
-			<ul>
-				<li> <h1><a href="nowshowing.php">Now Showing</a></h1></li>
-				<li> <h1><a href="comingsoon.php">Coming Soon</a></h1></li>
-			</ul>
-			</li>
-		<h1><a href="ticket.php">Ticket</a></h1>
-		<h1><a href="about.php">About</a></h1>
-	</div>
+
+<body id = "body">
+    <div id= "navcon">
+    </div>
+
+    
+    
+    <div id="homeContainer">    
+        <iframe class = "video"  src="https://www.youtube.com/embed/3Lyw4pz7z6c" frameborder="0" allowfullscreen></iframe>
+        <ul id="important-events">
+            <h1>Latest News</h1>
+            <li>
+               <p>Film of the week: SPECTRE</p>
+               <img src="images/spectre_2015_james_bond_007-wide.jpg">
+           </li>
+
+           <li class = "neverBullet">
+            <hr id="line">
+        </li>
+
+        <br/>
+        <li>
+            <p>Interstellar dominating the box office</p>
+            <img src="images/interstellar-2560x1440.jpg">
+        </li>
+
+    </ul>
+
+
+</div>
+
+
+
+
 
 </body>
+
 </html>
