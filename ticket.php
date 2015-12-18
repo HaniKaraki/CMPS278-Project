dbf9f76bdd5fde207da993699d1f0c0a16ed6e97
HaniK22 <Hani@fallound.com>
2015-12-18 11:17:50 +0200

    edited
    edited

--- ticket.php
+++ ticket.php
@@ -2,23 +2,21 @@
 <html>
 <head>
 <meta charset='utf-8'>
-<link href="styles/home.css" type="text/css" rel="stylesheet" /> 
+<link href="styles/navbar.css" type="text/css" rel="stylesheet" /> 
 
 </head>
 <body>
-	<div id="navcon">
-	<div class="nav">
-		<h1 class="navItem"><a href="home.php">Home</a></h1>
+	<div>
+		<h1><a href="home.php">Home</a></h1>
 		<ul>
-			<li><h1 class="navItem"><a href="#">Movies</a></h1>
+			<li><h1><a href="#">Movies</a></h1>
 			<ul>
-				<li> <h1 class="navItem"><a href="nowshowing.php">Now Showing</a></h1></li>
-				<li> <h1 class="navItem"><a href="comingsoon.php">Coming Soon</a></h1></li>
+				<li> <h1><a href="nowshowing.php">Now Showing</a></h1></li>
+				<li> <h1><a href="comingsoon.php">Coming Soon</a></h1></li>
 			</ul>
 			</li>
-		<h1 class="navItem"><a class="active" href="ticket.php">Ticket</a></h1>
-		<h1 class="navItem"><a href="about.php">About</a></h1>
-	</div>
+		<h1><a class="active" href="ticket.php">Ticket</a></h1>
+		<h1><a href="about.php">About</a></h1>
 	</div>
 
 </body>
