0b0ba14c0cf6e473e8b6c94a6015c1efbfb8a4d7
mwj95 <mwj_95@hotmail.com>
2015-12-18 11:13:32 +0200

    Nav
    edited nav

--- home.php
+++ home.php
@@ -6,7 +6,19 @@
 
 <body id = "body">
     <div id= "navcon">
-    </div>
+		<div class="nav">
+		<h1 class="navItem"><a class="active" href="home.php">Home</a></h1>
+			<ul>
+				<li><h1 class="navItem"><a href="#">Movies</a></h1>
+					<ul>
+						<li> <h1 class="navItem"><a href="nowshowing.php">Now Showing</a></h1></li>
+						<li> <h1 class="navItem"><a href="comingsoon.php">Coming Soon</a></h1></li>
+					</ul>
+				</li>
+			<h1 class="navItem"><a href="ticket.php">Ticket</a></h1>
+			<h1 class="navItem"><a href="about.php">About</a></h1>
+		</div>
+	</div>
 
     
     
